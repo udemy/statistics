cc794fd7514946550cd97d976baa4a061ff3ab1c
Robert Neal <robert.neal@udemy.com>
2018-08-31 09:55:53 -0700

    add required license information

--- src/main/scala/com/udemy/statistics/distribution/NormalInverseGamma.scala
+++ src/main/scala/com/udemy/statistics/distribution/NormalInverseGamma.scala
@@ -1,3 +1,19 @@
+/*
+Copyright 2018 Udemy, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package com.udemy.statistics.distribution
 
 import com.udemy.statistics._
@@ -30,8 +46,6 @@ case class NormalInverseGamma(μ: Double, ν: Double, α: Double, β: Double) {
   }
 
   def posterior(likelihood: NormalInverseGamma): NormalInverseGamma = {
-    // from https://en.wikipedia.org/wiki/Conjugate_prior
-
     if (likelihood.ν < 1) this
     else {
       if (ν == 0) likelihood
